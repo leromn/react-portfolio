c9e8b074a3dd4acfdeccc3316d3276292a03e138
Esrael Asefa <112072311+leromn@users.noreply.github.com>
2024-05-25 08:30:53 +0000

    youtube link and spline components base integration

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -5,19 +5,14 @@ import { Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 
-import './Works.css';
+import Spline from "@splinetool/react-spline";
 
-// Import ../../assets/recentprojects/
-import Portfolio from '../../assets/recentprojects/react-portfolio.png';
-import Veritru from '../../assets/recentprojects/veritru.png';
-import Lofo from '../../assets/recentprojects/lofo.png';
-import Startup from '../../assets/recentprojects/startup.png';
-import Lacalle from '../../assets/recentprojects/lacalle.png';
+import "./Works.css";
 
 const useStyles = makeStyles((theme) => ({
   main: {
-    maxWidth: '100vw',
-    marginTop: '3em',
+    maxWidth: "100vw",
+    marginTop: "3em",
     marginBottom: "auto",
   },
 }));
@@ -25,48 +20,48 @@ const useStyles = makeStyles((theme) => ({
 export const Works = () => {
   const classes = useStyles();
   const [projects, setProjects] = useState([
-    { 
+    {
       id: 1,
-      title: 'React Portfolio', 
+      title: "React Portfolio",
       description: `Designed and developed a ReactJS portfolio 
       with fancy 3D animations using Three.js for 
       the background element.`,
-      alter: 'React Portfolio',
-      image: `${Portfolio}`,
+      alter: "React Portfolio",
+      ytLink: "https://www.youtube.com/embed/hymtATx1QXw?si=4mmXBrnv1VOo7mhU",
     },
-    { 
+    {
       id: 2,
-      title: 'VeriTru Project', 
+      title: "VeriTru Project",
       description: `An advocacy project website built using
       MEAN stack with fact-checking tool to promote actions against
       fake news.`,
-      alter: 'VeriTru Project',
-      image: `${Veritru}`,
+      alter: "VeriTru Project",
+      ytLink: "https://www.youtube.com/embed/hymtATx1QXw?si=4mmXBrnv1VOo7mhU",
     },
-    { 
+    {
       id: 3,
-      title: 'LoFo Project', 
+      title: "LoFo Project",
       description: `Logistics and Forwarding website built using
       ReactJS to design and develop its front-end.`,
-      alter: 'LoFo Project',
-      image: `${Lofo}`,
+      alter: "LoFo Project",
+      ytLink: "https://www.youtube.com/embed/hymtATx1QXw?si=4mmXBrnv1VOo7mhU",
     },
-    { 
+    {
       id: 4,
-      title: 'Startup Project', 
+      title: "Startup Project",
       description: `A website portfolio project for the Startup Dev Team
       built using MEVN stack to demonstrate the CRUD capabilities of the tech stack.`,
-      alter: 'Startup Project',
-      image: `${Startup}`,
+      alter: "Startup Project",
+      ytLink: "https://www.youtube.com/embed/hymtATx1QXw?si=4mmXBrnv1VOo7mhU",
     },
-    { 
+    {
       id: 5,
-      title: 'LaCalle Cafe', 
+      title: "LaCalle Cafe",
       description: `A website project for the La Calle Cafe business
       built using Wordpress and PHP with integrated SEO tools to help
       the business ramp up its prospects and lead generation.`,
-      alter: 'Startup Project',
-      image: `${Lacalle}`,
+      alter: "Startup Project",
+      ytLink: "https://www.youtube.com/embed/hymtATx1QXw?si=4mmXBrnv1VOo7mhU",
     },
   ]);
 
@@ -74,20 +69,27 @@ export const Works = () => {
     <section id="works">
       <Container component="main" className={classes.main} maxWidth="md">
         {projects.map((project) => (
-          <div className="project" key={ project.id }>
+          <div className="project" key={project.id}>
             <div className="__img_wrapper">
-              <img src={ project.image } alt={ project.alter }/>
+              <iframe
+                src={project.ytLink}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe>
             </div>
             <div className="__content_wrapper">
               <h3 className="title">
-                <TextDecrypt text={ project.id + '. ' + project.title } />
+                <TextDecrypt text={project.id + ". " + project.title} />
               </h3>
-              <p className="description">
-                { project.description }
-              </p>
+              <p className="description">{project.description}</p>
             </div>
           </div>
         ))}
+        spline container is going to be placed here
+        <Spline scene="https://prod.spline.design/JT8tZe2b8jSxGm6n/scene.splinecode" />
       </Container>
     </section>
   );
